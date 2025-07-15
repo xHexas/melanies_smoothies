acb02441361cd069575104db11ade2806a411449
xHexas <xhexas@gmail.com>
2025-07-15 12:25:39 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,4 +41,4 @@ if ingredients_list:
         st.success('Your Smoothie is ordered ' +name_on_order +'!', icon="✅")
 st.text("Test123")
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
